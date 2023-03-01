97c22bf1b1f4f73e5be722340c758bf32c441f6f
mayurkasliwal <mayur.kasliwal@wipro.com>
2023-03-01 17:44:09 +0530

    added deploy

--- app.py
+++ app.py
@@ -1,49 +1,36 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # [START hello-app]
-import os
-from google.cloud import secretmanager
 from flask import Flask
+from google.cloud import secretmanager
 
 GOOGLE_CLOUD_PROJECT = "cloud-studio-369512"
-# SECRET_NAME = os.getenv("SECRET_NAME")
 SECRET_NAME="my_secret_value"
-secret_client = secretmanager.SecretManagerServiceClient()
+client = secretmanager.SecretManagerServiceClient()
 
 app = Flask('hello-cloudbuild')
-
-def extract_secert(project_id, secret_id):
-    """
-    Access the payload for the given secret version if one exists. The version
-    can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
-    """
-    version = "latest"
-    # Build the resource name of the secret version.
-    name = f"projects/{project_id}/secrets/{secret_id}/versions/{version}"
-
-    # Access the secret version.
-    response = secret_client.access_secret_version(request={"name": name})
-
-
-    # Verify payload checksum.
-    # crc32c = google_crc32c.Checksum()
-    # crc32c.update(response.payload.data)
-    # if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
-    #     print("Data corruption detected.")
-    #     return response
-
-    # Print the secret payload.
-    #
-    # WARNING: Do not print the secret in a production environment - this
-    # snippet is showing how to access the secret material.
-    payload = response.payload.data.decode("UTF-8")
-    print("Plaintext: {}".format(payload))
-    return payload
 
 
 @app.route('/')
 def hello():
-    secret_value = extract_secert(GOOGLE_CLOUD_PROJECT,SECRET_NAME)
-    greeting = f"Hello World! , this is {secret_value}"
-    return greeting
+  client = secretmanager.SecretManagerServiceClient()
+  name = f"projects/{GOOGLE_CLOUD_PROJECT}/secrets/{SECRET_NAME}/versions/latest"
+  response = client.access_secret_version(name=name)
+  payload = response.payload.data.decode("UTF-8")
+  greeting = f"Hello everyone! , this is {name}"
+  return greeting
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
